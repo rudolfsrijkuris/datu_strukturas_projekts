7f702e87e2ea369f622a8823c3985b3c5a3ccdc9
Rudolfs Rijkuris <rudolfs.rijkuris@gmail.com>
2025-05-12 19:51:12 +0300

    - Pievienota SportlandScraper klase, lai inicializētu Chrome WebDriver - WebDriver konfigurācija headless režīmā un papildus konfigurācija performances uzlabošanai.

--- sportland_scraper.py
+++ sportland_scraper.py
@@ -1,1 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+class SportlandScraper:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        self.driver = webdriver.Chrome(options=chrome_options)
